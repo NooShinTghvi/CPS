2cb5a06b9082186dc48e279e98880e9678f8e2d9
NooShin Taghavi <nooshin.tghvi@gmail.com>
2020-08-01 20:58:26 +0430

    initial project

---
+++ Arduion/photoresistor/photoresistor.ino
@@ -0,0 +1,15 @@
+// pin definitions
+#define FAULT_PIN 5
+//#define LRH A0
+#define TR_PIN 4
+#define HL_PIN 6
+#define TL_PIN 7
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
